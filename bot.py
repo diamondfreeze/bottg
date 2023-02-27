7b990a47044017e72c7fdc517b808dd951d77819
Никита <nik.vlz999@gmail.com>
2023-02-27 16:08:14 +0300

    вв

--- bot.py
+++ bot.py
@@ -7,13 +7,12 @@ bot = telebot.TeleBot(config.TOKEN)
 alpha = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
 @bot.message_handler(commands=['start'])
 def welcome(message):
-    global markup, item1,item2,item3,item4
     stik = open('welcome.webp', 'rb')
     bot.send_sticker(message.chat.id, stik)
     welcome_markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
     welcome_item1 = types.KeyboardButton("Привет!")
     welcome_markup.add(welcome_item1)
-    bot.send_message(message.chat.id, "Привет, {0.first_name}.\nМеня зовут <b>{1.first_name}</b>, и я не бот.".format(message.from_user, bot.get_me()), parse_mode='html', reply_markup=welcome_markup)
+    bot.send_message(message.chat.id, "Привет, {0.first_name}.\nМеня зовут <b>{1.first_name}</b>, и я не бот.\nПоздоровайся со мной!!!".format(message.from_user, bot.get_me()), parse_mode='html', reply_markup=welcome_markup)
 
 @bot.message_handler(content_types=['text'])
 def say(message):
@@ -26,7 +25,7 @@ def say(message):
         bass_markup.add(bass_item1,bass_item2,bass_item3)
         bot.send_message(message.chat.id, "Ну ок, привет, неуч.",parse_mode='html', reply_markup = bass_markup)
     elif message.text == "Придумай пароль!": bot.send_message(message.chat.id, "Ваш пароль {}.\nНикому его не говорите!!!".format(f"{choice(alpha)}{choice(alpha)}{choice(alpha)}{choice(alpha)}{choice(alpha)}{choice(alpha)}{choice(alpha)}{choice(alpha)}"))
-    elif message.text == "Расскажи мне историю": bot.send_message(message.chat.id, "Привет!")
+    elif message.text == "Расскажи мне историю": bot.send_message(message.chat.id, "Нет(@_@)")
     else: bot.send_message(message.chat.id, "Я вас не понять.")
 
 
