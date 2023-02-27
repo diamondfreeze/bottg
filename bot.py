03aebf60ee68a9c7fe7eb1679aef5600eccddf58
Maibenben <gleb.kazantsev.09@bk.ru>
2023-02-27 20:05:10 +0300

    Никита, научись оформлять код

--- bot.py
+++ bot.py
@@ -1,11 +1,11 @@
 import telebot
 from telebot import types
-import config
+import configtok
 from random import *
 from time import sleep
 
 mode = 'standart'
-bot = telebot.TeleBot(config.TOKEN)
+bot = telebot.TeleBot(configtok.TOKEN)
 alpha = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
 
 
