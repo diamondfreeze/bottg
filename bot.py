e5ec4b187366bb0c1941b82e09c4401c5270f586
Никита <nik.vlz999@gmail.com>
2023-02-28 13:58:48 +0300

    uoi

--- bot.py
+++ bot.py
@@ -6,7 +6,9 @@ from time import sleep
 
 mode = 'standart'
 bot = telebot.TeleBot(configtok.TOKEN)
-alpha = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
+alpha = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
+         'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',
+         'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
 stories = ['Как-то раз, гуляя в саду, я встретил Зелёнку...\n...\n...\n...\n'
            'а вообщем уже не важно\n...\nпотому что это был труп',
            'Эх помню историю про медвядя...\n...\nсейчас расскажу...\n...\n...\n...\n'
@@ -16,29 +18,30 @@ stories = ['Как-то раз, гуляя в саду, я встретил Зе
            'видимо у меня слишком прекрасный акцент\n...\n'
            'стейк между прочим был вкусный']
 
-@bot.message_handler(commands = ['start'])
+
+@bot.message_handler(commands=['start'])
 def welcome(message):
     stik = open('welcome.webp', 'rb')
     bot.send_sticker(message.chat.id, stik)
-    welcome_markup = types.ReplyKeyboardMarkup(resize_keyboard = True)
+    welcome_markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
     welcome_item1 = types.KeyboardButton("Привет!!!")
     welcome_markup.add(welcome_item1)
     bot.send_message(message.chat.id,
                      'Привет, {0.first_name}.\n'
                      'Меня зовут <b>{1.first_name}</b>.\n'
                      'Поздоровайся со мной!!!'.format(message.from_user, bot.get_me()),
-                     parse_mode = 'html',
-                     reply_markup = welcome_markup,
+                     parse_mode='html',
+                     reply_markup=welcome_markup,
                      )
 
 
-@bot.message_handler(content_types = ['text'])
+@bot.message_handler(content_types=['text'])
 def say(message):
     global mode
     if mode != 'active_bomber':
         if message.text == "Привет!!!" or message.text == "Назад":
             mode = 'standart'
-            bass_markup = types.ReplyKeyboardMarkup(row_width = 2,
+            bass_markup = types.ReplyKeyboardMarkup(row_width=2,
                                                     resize_keyboard=True,
                                                     )
             bass_item1 = types.KeyboardButton("Рандомный режим")
@@ -48,14 +51,14 @@ def say(message):
             bot.send_message(message.chat.id,
                              "Привет, {0.first_name}!!!".format(message.from_user),
                              parse_mode='html',
-                             reply_markup = bass_markup,
+                             reply_markup=bass_markup,
                              )
 
-        #randommode
+        # randommode
         elif message.text == "Рандомный режим" and mode == 'standart':
             mode = 'random'
-            random_markup = types.ReplyKeyboardMarkup(row_width = 2,
-                                                      resize_keyboard = True,
+            random_markup = types.ReplyKeyboardMarkup(row_width=2,
+                                                      resize_keyboard=True,
                                                       )
             random_item1 = types.KeyboardButton("Скажи рандом число")
             random_item2 = types.KeyboardButton("Придумай пароль")
@@ -63,8 +66,8 @@ def say(message):
             random_markup.add(random_item1, random_item2, random_item3)
             bot.send_message(message.chat.id,
                              "RANDOM MODE ACTIVATED",
-                             parse_mode = 'html',
-                             reply_markup = random_markup,
+                             parse_mode='html',
+                             reply_markup=random_markup,
                              )
 
         elif message.text == "Скажи рандом число" and mode == 'random':
@@ -72,30 +75,31 @@ def say(message):
                              "Хорошо, хорошо, ваше число - {}".format(randint(0, 10000)))
 
         elif message.text == "Придумай пароль" and mode == 'random':
-            bot.send_message(message.chat.id, "Ваш пароль {}\nНикому его не говорите\nЯ итак уже всем скинул".format(f"{choice(alpha)}{choice(alpha)}{choice(alpha)}{choice(alpha)}{choice(alpha)}{choice(alpha)}{choice(alpha)}{choice(alpha)}"))
+            bot.send_message(message.chat.id, "Ваш пароль {}\nНикому его не говорите\nЯ итак уже всем скинул".format(
+                f"{choice(alpha)}{choice(alpha)}{choice(alpha)}{choice(alpha)}{choice(alpha)}{choice(alpha)}{choice(alpha)}{choice(alpha)}"))
 
-        #bombermode
+        # bombermode
         elif message.text == "Кинуть бомбер" and mode == 'standart':
             mode = 'bomber'
-            bomber_markup = types.ReplyKeyboardMarkup(resize_keyboard = True)
+            bomber_markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
             bomber_item1 = types.KeyboardButton('Назад')
             bomber_markup.add(bomber_item1)
             bot.send_message(message.chat.id, "Введите номер телефона жертвы.\nПример:8 987-654-32-10",
-                             parse_mode = 'html',
-                             reply_markup = bomber_markup,
+                             parse_mode='html',
+                             reply_markup=bomber_markup,
                              )
 
         elif message.text != 'Назад' and mode == 'bomber':
             mode = 'active_bomber'
-            clear_markup = types.ReplyKeyboardMarkup(row_width = 2,
-                                                     resize_keyboard = True,
+            clear_markup = types.ReplyKeyboardMarkup(row_width=2,
+                                                     resize_keyboard=True,
                                                      )
-            clear = types.KeyboardButton(f"{choice(['Пожалуйста, остановись','Пощади!','NO, GOD, NOO!'])}")
+            clear = types.KeyboardButton(f"{choice(['Пожалуйста, остановись', 'Пощади!', 'NO, GOD, NOO!'])}")
             clear_markup.add(clear)
             bot.send_message(message.chat.id,
                              "Кидать бомберы плохо, поэтому получай!",
-                             parse_mode = 'html',
-                             reply_markup = clear_markup)
+                             parse_mode='html',
+                             reply_markup=clear_markup)
             clear_markup = types.ReplyKeyboardRemove()
             bomber_markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
             bomber_item1 = types.KeyboardButton("Назад")
@@ -104,25 +108,26 @@ def say(message):
 
             for i in range(50):
                 bot.send_message(message.chat.id,
-                                 f"{choice(['Так делать нельзя!','Ты понял, что нельзя кидать бомберы?','В следующий раз не кидай бомберы!'])}")
+                                 f"{choice(['Так делать нельзя!', 'Ты понял, что нельзя кидать бомберы?', 'В следующий раз не кидай бомберы!'])}")
 
             mode = 'standart'
             bot.send_message(message.chat.id,
                              '\nНадеюсь ты выучил урок!',
-                             parse_mode = 'html',
-                             reply_markup = bomber_markup,
+                             parse_mode='html',
+                             reply_markup=bomber_markup,
                              )
 
-        #storymode
-        elif message.text == 'Расскажи мне историю' and mode == 'standart':
+        # storymode
+        elif message.text == 'Расскажи мне историю' or message.text == 'Расскажи ещё одну историю!':
             mode = 'story'
-            story_markup = types.ReplyKeyboardMarkup(resize_keyboard = True)
+            story_markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
             story_item1 = types.KeyboardButton('Назад')
-            story_markup.add(story_item1)
+            story_item2 = types.KeyboardButton('Расскажи ещё одну историю!')
+            story_markup.add(story_item1, story_item2)
             bot.send_message(message.chat.id,
                              choice(stories),
-                             parse_mode = 'html',
-                             reply_markup = story_markup,
+                             parse_mode='html',
+                             reply_markup=story_markup,
                              )
             mode = 'standart'
 
@@ -131,4 +136,5 @@ def say(message):
                              'У меня нет такой функции\n'
                              'Обращайтесь к админам DiamondFreeze и NikitaPROGOD!')
 
-bot.polling(none_stop = True)
+
+bot.polling(none_stop=True)
